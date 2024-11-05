2f8f7c5d845886fe0b5ef241bc8f58f31cb10a0b
Justin <94499968+jzaager@users.noreply.github.com>
2024-11-04 20:21:53 -0500

    Add main and TextNode

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+def main():
+    dummy_node = TextNode("This is a text node", TextType.BOLD, "http.boot.dev")
+    print(dummy_node)
+
+if __name__ == "__main__":
+    main()
